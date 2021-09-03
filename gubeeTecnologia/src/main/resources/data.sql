7b834457c75fd28e128e32fb5ec96373da67d7d9
jessemusic <jmc_cursos@hotmail.com>
2021-09-03 14:54:03 -0300

    Desenvolvendo a api rest da gubee tecnologia produtos no data.sql modificados

--- gubeeTecnologia/src/main/resources/data.sql
+++ gubeeTecnologia/src/main/resources/data.sql
@@ -1,5 +1,5 @@
 
 INSERT INTO PRODUTO(nome, decricao,mecardo_alvo,data_criacao,tecnologia_utilizada) VALUES('Spring Boot', 'FrameWork para desenvolvimento', 'Programadores em geral','2021-09-02T08:11:51.2789887', 'Spring Tool Suite');
-INSERT INTO PRODUTO(nome, decricao,mecardo_alvo,data_criacao,tecnologia_utilizada) VALUES('JPA', 'FrameWork para desenvolvimento', 'Programadores em geral','2021-09-02T08:11:51.2789887', 'Spring Data JPA');
+INSERT INTO PRODUTO(nome, decricao,mecardo_alvo,data_criacao,tecnologia_utilizada) VALUES('JPA', 'FrameWork para desenvolvimento', 'Desenvolvedor JAVA','2021-09-02T08:11:51.2789887', 'Spring Data JPA');
 INSERT INTO PRODUTO(nome, decricao,mecardo_alvo,data_criacao,tecnologia_utilizada) VALUES('Mysql', 'Banco de dados', 'Programadores em geral','2021-09-02T08:11:51.2789887', 'HEIDI');
-INSERT INTO PRODUTO(nome, decricao,mecardo_alvo,data_criacao,tecnologia_utilizada) VALUES('Oracle Code', 'Prioritizing the health and safety of our attendees','Programadores em geral','2021-09-02T08:11:51.2789887', 'MYSQL , NOSQL');
+INSERT INTO PRODUTO(nome, decricao,mecardo_alvo,data_criacao,tecnologia_utilizada) VALUES('Oracle Code', 'Prioritizing the health and safety of our attendees','Programador banco de dados','2021-09-02T08:11:51.2789887', 'MYSQL , NOSQL');
